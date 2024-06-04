5051b1f0b63541923fd882e2ea13a52f83690f35
AugustoFons <augustofonsdev@gmail.com>
2024-06-04 17:21:57 -0300

    edittt

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,14 @@ import NoteViewer from "@/components/Editor";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col justify-between p-24">
-        <div className="note">
-        <NoteViewer/>
-        </div>
+      <main className="flex min-h-screen flex-col justify-between py-24 px-8">
+      <div className="note">
+      <NoteViewer/>
+      </div>
       <div style={{display: 'flex', justifyContent: 'flex-start'}}>
         <section style={{display: 'flex', zIndex:1}}>
             <div className='anillado' style={{position: 'relative', flex:1}}>
-              <img src={'/assets/anillados.png'} alt='' style={{height:'100%', backgroundColor:'#FADD99', zIndex:0}}/>
+              <img src={'/assets/anillados.png'} alt='' style={{height:'100%', backgroundColor:'transparent', zIndex:0}}/>
               <div className='cuaderno'></div>
             </div>
             <div className="hoja">
@@ -18,12 +18,12 @@ export default function Home() {
                 {
                   Array(31).fill().map((_, i) => (
                     <div key={i} className='renglon content-end overflow-visible '>
-                    <input
+    {/*                <input
                           type="text"
                           placeholder=""
                           required
                           className="relative -bottom-[8px] caret-blue700 outline-none text-2xl h-[26px] bg-transparent overflow-visible text-blue-700 w-full"
-                          />
+                          /> */}
                     </div>                  
                   ))
                 }  
