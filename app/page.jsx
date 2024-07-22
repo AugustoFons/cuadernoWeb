771f66fe3a030258546541fea0e4cc10ef0a48e9
augustoFons <augustofonsdev@gmail.com>
2024-07-21 21:40:40 -0300

    tool

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsLoading(false);
-    }, 3800); // Simula 2 segundos de carga
+    }, 3600); // Simula 2 segundos de carga
 
     return () => clearTimeout(timeout);
   }, []);
@@ -26,45 +26,44 @@ export default function Home() {
     {isLoading ? (
       <div className=' w-full flex justify-center'>
           <Loading />
-
       </div>
         )
         : (
-      <main className="flex">
-          <div className='bg-white w-[109px] h-[104px] z-50 rounded-full absolute -top-[-13px] border-2 border-black logo'>
+      <main className="flex w-[1460px]">
+        <div className='bg-white w-[126px] h-[122px] z-50 rounded-full absolute -top-[-17.4px] border-4 border-[#020248] logo'>
             <Image 
             src='/assets/escc.png'
             width={440}
             height={242}
             alt="line"
-            className="h-[62px] w-[99px] absolute top-6 left-[1px] "
+            className="h-[66px] w-[112px] absolute top-7 left-[0px] "
             quality={100}
             />
-            </div>
+          </div>
           <>
           <div className="note">
             <NoteViewer />
           </div>
-      <div className='flex justify-start mt-6 w-full'>
-        <section style={{display: 'flex', zIndex:1}}> 
-            <div className='' style={{position: 'relative', flex:1}}>
-              <div className='cuaderno'></div>
-            </div>
-            <div className='anillado' style={{position: 'relative', flex:1}}>
-            <img src={'/assets/an.png'} alt='' style={{height:'100%', backgroundColor:'transparent', zIndex:0}}/>
-            </div>
-            <div className="hoja">
-            <span className='encabezado'></span>
-                {
-                  Array(31).fill().map((_, i) => (
-                    <div key={i} className='renglon content-end overflow-visible '>
-                    </div>                  
-                  ))
-                }  
-            </div>
-        </section>
-    </ div>
-    </>
+          <div className='flex justify-start mt-12 w-full'>
+            <section style={{display: 'flex', zIndex:1}}> 
+                <div className='' style={{position: 'relative', flex:1}}>
+                  <div className='cuaderno'></div>
+                </div>
+                <div className='anillado' style={{position: 'relative', flex:1}}>
+                <img src={'/assets/an.png'} alt='' style={{height:'100%', backgroundColor:'transparent', zIndex:0}}/>
+                </div>
+                <div className="hoja">
+                <span className='encabezado'></span>
+                    {
+                      Array(31).fill().map((_, i) => (
+                        <div key={i} className='renglon content-end overflow-visible '>
+                        </div>                  
+                      ))
+                    }  
+                </div>
+            </section>
+              </ div>
+          </>
     </main>
     )}
 
